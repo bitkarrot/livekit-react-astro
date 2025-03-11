d9cf2ce91af711fa77a1793c8790b860468d322c
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2025-03-10 23:57:08 -0700

    basic works

--- src/components/QuickComponent.tsx
+++ src/components/QuickComponent.tsx
@@ -5,33 +5,58 @@ import {
     ParticipantTile,
     RoomAudioRenderer,
     useTracks,
+    formatChatMessageLinks,
+    VideoConference,
   } from '@livekit/components-react';
   
-  import '@livekit/components-styles';
-  
-  import { Track } from 'livekit-client';
+  import '@livekit/components-styles';  
+  import React from 'react';
+
+  import { DebugMode } from '../../lib/Debug';
+
+  import { Track,
+        type VideoCodec,
+        LogLevel,
+  } from 'livekit-client';
   
   const serverUrl = 'wss://thehive-g3v6mhu7.livekit.cloud';
   const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDE3NTExNjAsImlzcyI6IkFQSW1RV0daOFRyQ0VkdiIsIm5hbWUiOiJ0ZXN0X3VzZXIiLCJuYmYiOjE3NDE2NjQ3NjAsInN1YiI6InRlc3RfdXNlciIsInZpZGVvIjp7InJvb20iOiJ0ZXN0X3Jvb20iLCJyb29tSm9pbiI6dHJ1ZX19.7ct4huaMpXlnPdfqf3gZjn3twHTRlAcK_BIANbdhXIw';
   
   export default function Page() {
+    const handleError = React.useCallback((error: Error) => {
+        console.error(error);
+        alert(`Encountered an unexpected error, check the console logs for details: ${error.message}`);
+      }, []);
+    
+    const handleOnLeave = React.useCallback(() => {
+        window.location.href = '/exit'; // Redirect to home
+      }, []);
+    
     return (
       <LiveKitRoom
         video={true}
         audio={true}
         token={token}
         serverUrl={serverUrl}
+        onDisconnected={handleOnLeave}
+        onError={handleError}
         // Use the default LiveKit theme for nice styles.
         data-lk-theme="default"
         style={{ height: '100vh' }}
       >
         {/* Your custom component with basic video conferencing functionality. */}
         <MyVideoConference />
+
+        {/* <VideoConference
+          chatMessageFormatter={formatChatMessageLinks}
+        /> */}
+
         {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
         <RoomAudioRenderer />
         {/* Controls for the user to start/stop audio, video, and screen
         share tracks and to leave the room. */}
         <ControlBar />
+        {/* <DebugMode logLevel={LogLevel.debug} /> */}
       </LiveKitRoom>
     );
   }
